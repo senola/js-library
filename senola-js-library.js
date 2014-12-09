e97292f6744b04c22fb96263621cae2a0c3b48f2
zhijun_wang <zhijun_wang@kingdee.com>
2014-12-09 14:16:56 +0800

    ajax
    ajax

--- senola-js-library.js
+++ senola-js-library.js
@@ -1,26 +1,25 @@
 /**
  *  author: senola
  *  version: 0.0.1
- *  createTime: 2014-12-9  
+ *  createTime: 2014-12-9
  *  description: my own native javascript library
  *  repository: https://github.com/senola/js-library
  *  blog: senola.github.io
-**/
-
+ **/
 var senolaUtils = {
-	  /**
-	   * 获取浏览器信息
-		 **/
-	  getBrowserInfo: function() { // 各个浏览器判断
+    /**
+     * 获取浏览器信息
+     **/
+    getBrowserInfo: function() { // 各个浏览器判断
         var Sys = {};
         var ua = navigator.userAgent.toLowerCase(); //获取 userAgent 值
         var s;
-        (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? Sys.ie = s[1] : //IE 11
-        (s = ua.match(/msie ([\d.]+)/)) ? Sys.ie = s[1] :
-        (s = ua.match(/firefox\/([\d.]+)/)) ? Sys.firefox = s[1] :
-        (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] :
-        (s = ua.match(/opera.([\d.]+)/)) ? Sys.opera = s[1] :
-        (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
+        (s = ua.match(/rv:([\d.]+)\) like gecko/)) ? Sys.ie = s[1]: //IE 11
+            (s = ua.match(/msie ([\d.]+)/)) ? Sys.ie = s[1] :
+            (s = ua.match(/firefox\/([\d.]+)/)) ? Sys.firefox = s[1] :
+            (s = ua.match(/chrome\/([\d.]+)/)) ? Sys.chrome = s[1] :
+            (s = ua.match(/opera.([\d.]+)/)) ? Sys.opera = s[1] :
+            (s = ua.match(/version\/([\d.]+).*safari/)) ? Sys.safari = s[1] : 0;
 
         if (Sys.ie) return ('IE: ' + Sys.ie);
         if (Sys.firefox) return ('Firefox: ' + Sys.firefox);
@@ -29,12 +28,12 @@ var senolaUtils = {
         if (Sys.safari) return ('Safari: ' + Sys.safari);
     },
 
-		/**
-		 *  获取js宿主环境信息
-		 **/
-		versions: function () {
-        var u = window.navigator.userAgent, 
-           app = window.navigator.appVersion;
+    /**
+     *  获取js宿主环境信息
+     **/
+    versions: function() {
+        var u = window.navigator.userAgent,
+            app = window.navigator.appVersion;
         return {
             trident: u.indexOf('Trident') > -1,
             presto: u.indexOf('Presto') > -1,
@@ -50,31 +49,124 @@ var senolaUtils = {
         };
     }(),
 
-		/**
-		 *   判断是否是PC端，返回true表示“是”，返回false,表示“否”
-		 **/
-	  isPC : function () {                            
-				var ua = navigator.userAgent;
-				var agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");
-				var flag = true;
-				for (var i = 0, j = agents.length; i < j ; i ++ ){
-				    if (ua.indexOf(agents[i]) != -1) {
-				           flag = false; 
-				           break;
-				    }
-				}
-				return flag;
+    /**
+     *   判断是否是PC端，返回true表示“是”，返回false,表示“否”
+     **/
+    isPC: function() {
+        var ua = navigator.userAgent;
+        var agents = new Array("Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod");
+        var flag = true;
+        for (var i = 0, j = agents.length; i < j; i++) {
+            if (ua.indexOf(agents[i]) != -1) {
+                flag = false;
+                break;
+            }
+        }
+        return flag;
     },
+    /**
+     *  原生js Ajax请求
+     *  用法： ajax_opt({
+     *            "method" : "post",
+     *            "url" : "dem0o.php",
+     *            "data" : {
+     *                  "name" : "senola",
+     *                  "age" : 25,
+     *                  "num" : "1234567890"
+     *              },
+     *            "success" : function(data){
+     *                  alert(data);
+     *            },
+     *            "Error" : function(text){
+     *               alert(text);
+     *             },
+     *             "async" : false 
+     *         });
+     *
+     **/
+    function ajax_opt(obj) {
+        var xhr = senolaUtils.createXHR(); //获取xhr对象
+        obj.url = obj.url; // 清除缓存
+        obj.data = senolaUtils.paramsEscape(obj.data); // 转义字符串
+        if(obj.method === "get"){      // 判断使用的是否是get方式发送
+            obj.url += obj.url.indexOf("?") == "-1" ? "?" + obj.data : "&" + obj.data;
+        }
+        // 异步
+        if(obj.async === true){
+            // 异步的时候需要触发onreadystatechange事件
+            xhr.onreadystatechange = function(){
+                // 执行完成
+                if(xhr.readyState == 4){
+                    callBack();
+                }
+            }
+        }
+        xhr.open(obj.method, obj.url, obj.async);  // false是同步 true是异步 // "demo.php?rand="+Math.random()+"&name=ga&ga",
+        if(obj.method === "post"){
+            xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+            xhr.send(obj.data);
+        }else{
+            xhr.send(null);
+        }
+        // xhr.abort(); // 取消异步请求
+        // 同步
+        if(obj.async === false){
+            callBack();
+        }
+        // 返回数据
+        function callBack(){
+            // 判断是否返回正确
+            if(xhr.status == 200){
+                obj.success(xhr.responseText);
+            }else{
+                obj.Error("获取数据失败，错误代号为："+xhr.status+"错误信息为："+xhr.statusText);
+            }
+        }
+    }, 
+    /**
+     *  创建兼容各浏览器的xhr对象
+     **/
+    function createXHR() {
+        if(typeof XMLHttpRequest != "undefined"){ // 非IE6浏览器
+            return new XMLHttpRequest();
+        }else if(typeof ActiveXObject != "undefined"){   // IE6浏览器
+            var version = [
+                        "MSXML2.XMLHttp.6.0",
+                        "MSXML2.XMLHttp.3.0",
+                        "MSXML2.XMLHttp",
+            ];
+            for(var i = 0; i < version.length; i++){
+                try{
+                    return new ActiveXObject(version[i]);
+                }catch(e){
+                    //跳过
+                }
+            }
+        }else{
+            throw new Error("您的系统或浏览器不支持XHR对象！");
+        }
+    }
+    /**
+     *  转义字符
+     **/
+    function paramsEscape(data){
+        var arr = [];
+        for(var i in data){
+            arr.push(encodeURIComponent(i) + "=" + encodeURIComponent(data[i]));
+        }
+        return arr.join("&");
+    }
     /**
      *  获取[under,over]之间的随机数。若只传入一个参数num则返回的结果表示[0,num]间的随机数
      **/
-    getRandomNum : function (under, over){
-			switch (arguments.length){ //判断参数的长度
-					case 1: 
-					    return parseInt(Math.random() * under + 1);
-					case 2: 
-					    return parseInt(Math.random() * (over - under + 1) + under);
-					default: return 1;
-			}
-		}
+    getRandomNum: function(under, over) {
+        switch (arguments.length) { //判断参数的长度
+            case 1:
+                return parseInt(Math.random() * under + 1);
+            case 2:
+                return parseInt(Math.random() * (over - under + 1) + under);
+            default:
+                return 1;
+        }
+    }
 }
